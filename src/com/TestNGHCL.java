1fcfc7876bb1c8e437189a46fba0f2c6c8776062
apurbaniit <apurbaasn@gmail.com>
2021-12-24 16:19:48 +0530

    Update TestNGHCL.java

--- src/com/TestNGHCL.java
+++ src/com/TestNGHCL.java
@@ -5,6 +5,6 @@ public class TestNGHCL {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my Main");
-		System.out.println("Welcome to java")
+		System.out.println("Welcome to java");
 	}
 }
